da46cbf19edb87e0fc14b73cf3c68b1d7f8adbe0
Jordan Christiansen <xordspar0@gmail.com>
2015-07-14 19:49:09 -0500

    Added LDAC classification to analyze_noaa.py (plus some graphs)

--- analyze_noaa.py
+++ analyze_noaa.py
@@ -16,12 +16,13 @@ data = np.loadtxt('data/StormEvents_combined_d2010.csv', delimiter='\t')
 position = np.hstack((data[:, 2:3], data[:, 1:2]))
 categories = np.array(data[:, 3:4].flatten(), dtype=np.int)
 
-# Show all of the data.
-plt.scatter(position[:, 0:1], position[:, 1:2], c=categories)
-plt.show()
+# Plot all of the data.
+ax1 = plt.subplot2grid((2,3), (0,0), rowspan=2, colspan=2,
+        title='All Storms', aspect='equal')
+ax1.scatter(position[:, 0:1], position[:, 1:2], c=categories)
 
 # Take a random sample and perform an LDA analysis.
-np.random.seed(0)
+#np.random.seed(0)
 sample_size = 500
 sample_choice = np.random.choice(range(len(data)), size=sample_size)
 sample_pos = np.zeros((sample_size, 2))
@@ -32,14 +33,42 @@ for i, choice in enumerate(sample_choice):
     sample_pos[i][1] = position[choice][1]
     sample_cat[i] = categories[choice]
 
-plt.scatter(sample_pos[:, 0:1], sample_pos[:, 1:2], c=sample_cat)
-plt.show()
+# Plot the sample data
+ax2 = plt.subplot2grid((2,3), (0,2),
+        title='Random Sample (n={})'.format(sample_size), aspect='equal')
+ax2.scatter(sample_pos[:, 0:1], sample_pos[:, 1:2], c=sample_cat)
 
+# Use mlpy's LDAC to learn and classify the data
 ldac = mlpy.LDAC()
 ldac.learn(sample_pos, sample_cat)
 
-for storm in sample_pos:
-    ldac.pred(storm)
+classifications = np.zeros(sample_size, np.int)
+for i, storm in enumerate(sample_pos):
+    classifications[i] = ldac.pred(storm)
+
+# Check the classification against the actual storm types
+correct = 0
+incorrect = 0
+correctness_matrix = []
+for i in range(sample_size):
+    if classifications[i] == sample_cat[i]:
+        correct += 1
+        correctness_matrix.append('g')
+    else:
+        incorrect += 1
+        correctness_matrix.append('r')
+
+ax3 = plt.subplot2grid((2,3), (1,2),
+        title='Correct/Incorrect Classifications', aspect='equal')
+ax3.scatter(sample_pos[:, 0:1], sample_pos[:, 1:2], c=correctness_matrix)
+
+# Show results
+print("Sample size: {}".format(sample_size))
+print("Number of correct classifications: {} ({}%)".format(
+        correct, (100*correct/sample_size) ))
+print("Number of incorrect classifications: {} ({}%)".format(
+        incorrect, (100*incorrect/sample_size) ))
+plt.show()
 
 # Might need this later
 event_types = ['ASTRONOMICAL LOW TIDE', 'AVALANCHE', 'BLIZZARD',
