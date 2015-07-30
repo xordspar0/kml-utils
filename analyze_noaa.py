870ff3e8837fcc6784ec5cec21b5904810e225f3
Jordan Christiansen <xordspar0@gmail.com>
2015-07-30 15:32:53 -0500

    Finished dividing the classifiers into separate functions

--- analyze_noaa.py
+++ analyze_noaa.py
@@ -12,10 +12,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import mlpy
 
+SAMPLE_SIZE = 500
+
 def main():
     # Load the data.
     data = np.loadtxt('data/StormEvents_combined_d2010.csv', delimiter='\t')
-    position = np.hstack((data[:, 2:3], data[:, 1:2]))
+    locations = np.hstack((data[:, 2:3], data[:, 1:2]))
     categories = np.array(data[:, 3], dtype=np.int)
     event_types = ['ASTRONOMICAL LOW TIDE', 'AVALANCHE', 'BLIZZARD',
             'COASTAL FLOOD', 'COLD/WIND CHILL', 'DEBRIS FLOW', 'DENSE FOG',
@@ -32,31 +34,30 @@ def main():
 
     # Take a random sample and perform an LDA analysis.
     np.random.seed(0)
-    sample_size = 500
-    sample_choice = np.random.choice(range(len(data)), size=sample_size)
-    sample_pos = np.zeros((sample_size, 2))
-    sample_cat = np.zeros(sample_size, np.int)
+    sample_choice = np.random.choice(range(len(data)), size=SAMPLE_SIZE)
+    sample_loc = np.zeros((SAMPLE_SIZE, 2))
+    sample_cat = np.zeros(SAMPLE_SIZE, np.int)
     sample_cat_counts = np.zeros(len(event_types), np.int)
 
     for i, choice in enumerate(sample_choice):
-        sample_pos[i][0] = position[choice][0]
-        sample_pos[i][1] = position[choice][1]
+        sample_loc[i][0] = locations[choice][0]
+        sample_loc[i][1] = locations[choice][1]
         sample_cat[i] = categories[choice]
         sample_cat_counts[categories[choice]] += 1
 
     # Plot the sample data.
     ax1 = plt.subplot2grid((2,2), (0,0),
-            title='Random Sample (n={})'.format(sample_size))
-    ax1.scatter(sample_pos[:, 0:1], sample_pos[:, 1:2], c=sample_cat)
+            title='Random Sample (n={})'.format(SAMPLE_SIZE))
+    ax1.scatter(sample_loc[:, 0:1], sample_loc[:, 1:2], c=sample_cat)
 
     # Classify the data
-    ldac()
+    classifications = ldac(sample_loc, sample_cat)
 
     # Check the classification against the actual storm types.
     correct_total = 0
-    # The positions of the correctly and incorrectly classified storms:
-    correct_pos = []
-    incorrect_pos = []
+    # The locations of the correctly and incorrectly classified storms:
+    correct_loc = []
+    incorrect_loc = []
     # The evaluations array contains the total number of True Positives (0),
     # True Negatives (1), False Positives (2), and False Negatives (3) for each
     # event type:
@@ -64,9 +65,9 @@ def main():
     for i, classification in enumerate(classifications):
         if classification == sample_cat[i]:
             correct_total += 1
-            correct_pos.append(sample_pos[i])
+            correct_loc.append(sample_loc[i])
         else:
-            incorrect_pos.append(sample_pos[i])
+            incorrect_loc.append(sample_loc[i])
 
         # Positive
         for event_type_index, event_type in enumerate(event_types):
@@ -86,15 +87,15 @@ def main():
                 else:
                     evaluations[event_type_index][3] += 1
 
-    correct_pos = np.array(correct_pos)
-    incorrect_pos = np.array(incorrect_pos)
+    correct_loc = np.array(correct_loc)
+    incorrect_loc = np.array(incorrect_loc)
     evaluations = np.array(evaluations)
 
     # Plot the correctness data.
     ax2 = plt.subplot2grid((2,2), (0,1),
             title='Correct/Incorrect Classifications')
-    ax2.scatter(incorrect_pos[:, 0:1], incorrect_pos[:, 1:2], c='b')
-    ax2.scatter(correct_pos[:, 0:1], correct_pos[:, 1:2], c='y')
+    ax2.scatter(incorrect_loc[:, 0:1], incorrect_loc[:, 1:2], c='b')
+    ax2.scatter(correct_loc[:, 0:1], correct_loc[:, 1:2], c='y')
     ax2.legend(['incorrect', 'correct'])
 
     # Plot the True/False Positives/Negatives
@@ -107,9 +108,9 @@ def main():
     ax3.legend(['True Positives', 'False Positives', 'False Negatives', 'True Negatives'], loc=2)
 
     # Show result and graphs
-    print('Sample size: {}'.format(sample_size))
+    print('Sample size: {}'.format(SAMPLE_SIZE))
     print('Number of total correct classifications: {} ({:.2%})'.format(
-            correct_total, (correct_total/sample_size) ))
+            correct_total, (correct_total/SAMPLE_SIZE) ))
     print()
     for i, event_type in enumerate(event_types):
         if sample_cat_counts[i] > 0:
@@ -120,22 +121,26 @@ def main():
     print('Storm types not listed were not present in the sample.')
     plt.show()
 
-def lda():
+def ldac(locations, categories):
     # Use mlpy's Linear Discriminant Analysis to learn and classify the data.
     ldac = mlpy.LDAC()
-    ldac.learn(sample_pos, sample_cat)
+    ldac.learn(locations, categories)
 
-    classifications = np.zeros(sample_size, np.int)
-    for i, storm in enumerate(sample_pos):
+    classifications = np.zeros(SAMPLE_SIZE, np.int)
+    for i, storm in enumerate(locations):
         classifications[i] = ldac.pred(storm)
 
-def knn():
+    return classifications
+
+def knn(locations, categories):
     # Use mlpy's k-Nearest Neighbor to learn and classify the data.
     knn = mlpy.KNN(k=3)
-    knn.learn(sample_pos, sample_cat)
+    knn.learn(locations, categories)
 
-    classifications = np.zeros(sample_size, np.int)
-    for i, storm in enumerate(sample_pos):
+    classifications = np.zeros(SAMPLE_SIZE, np.int)
+    for i, storm in enumerate(locations):
         classifications[i] = knn.pred(storm)
+
+    return classifications
 
 main()
